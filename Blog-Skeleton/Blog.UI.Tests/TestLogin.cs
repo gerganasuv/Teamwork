25ddb8dff1023b8b50ecc53402d13e209450fb0a
Tsve-ti <moito_id@yahoo.com>
2017-05-21 00:22:09 +0300

    Screenshots

--- Blog-Skeleton/Blog.UI.Tests/TestLogin.cs
+++ Blog-Skeleton/Blog.UI.Tests/TestLogin.cs
@@ -2,9 +2,12 @@
 using Blog.UI.Tests.Pages.Login;
 using Blog.UITests.Models;
 using NUnit.Framework;
+using NUnit.Framework.Interfaces;
 using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +27,25 @@ namespace Blog.UI.Tests
 
 
         }
+
+        [TearDown]
+        public void CleanUp()
+        {
+
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+
+
+                string filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory) + ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".jpg";
+
+                var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot();
+
+                screenshot.SaveAsFile(filename, ScreenshotImageFormat.Jpeg);
+
+            }
+        }
+
         [Test, Property("Priority", 1)]
         [Author("TSV")]
         public void ValidLogIn()
@@ -48,6 +70,32 @@ namespace Blog.UI.Tests
             loginPage.NavigatetoBlogLogIn();
             loginPage.FillLogInForm(logIn);
             loginPage.AssertNoEmailAddressDetected("The Email field is required");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("TSV")]
+        public void ToFail()
+        {
+            var loginPage = new LoginPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("No Email Address");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertNoEmailAddressDetected("Password");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("TSV")]
+        public void ValidLogInToFail()
+        {
+            // IWebDriver driver = BrowserHost.Instance.Application.Browser;
+
+            var loginPage = new LoginPage(this.driver);
+            LogIn user = new LogIn("Tsvetelina@abv.bg", "123456");
+            //var logIn = AccessExcelData.GetTestData("ValidLogin");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInFormHardCode(user);
+            loginPage.AssertValidLogIn1(("Goodbye"));
+            loginPage.AssertValidLogIn2("Log off");
         }
 
     }
